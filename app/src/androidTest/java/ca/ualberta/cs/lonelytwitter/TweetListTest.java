d3e195dbd1f1dad345fa2bace4c9618c218879d1
AkashPatelUAlberta <akpatel@ualberta.ca>
2017-10-11 18:28:03 -0600

    LabExercise

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,45 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by apate on 2017-10-11.
+ */
+
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+    public TweetListTest() {
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+//        testHasTweet();
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("adding a tweet");
+        tweets.add(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+    }
+
+    public void testDelete() {
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        tweets.add(tweet);
+        tweets.delete(tweet);
+        assertFalse(tweets.hasTweet(tweet));
+    }
+
+    public void testHasTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        assertTrue(list.hasTweet(tweet));
+    }
+
+    public void testGetTweet() {
+        TweetList list = new TweetList();
+        Tweet tweet = new NormalTweet("test");
+        list.add(tweet);
+        Tweet returnedTweet = list.getTweet(0);
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+    }
+
+}
